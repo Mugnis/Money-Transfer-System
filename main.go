7a1dabcab3958cb470f50a7f3023fdbe97360ef6
Mugnis <137095964+Mugnis@users.noreply.github.com>
2026-09-26 23:07:48 +0600

    Update main.go

--- main.go
+++ main.go
@@ -33,9 +33,10 @@ func (p *PaymentSystem) AddTransaction(t Transaction) {
 }
 
 func (p *PaymentSystem) ProcessingTransactions(t Transaction) error {
-	user1, ok := p.Users[t.FromID]
+	user1, ok1 := p.Users[t.FromID]
+	user2, ok2 := p.Users[t.ToID]
 
-	if !ok {
+	if !ok1 || !ok2 {
 		return errors.New("User not found")
 	}
 
@@ -43,11 +44,6 @@ func (p *PaymentSystem) ProcessingTransactions(t Transaction) error {
 		return err
 	}
 
-	user2, ok := p.Users[t.ToID]
-
-	if !ok {
-		return errors.New("User not found")
-	}
 	user2.Deposit(t.Amount)
 
 	return nil
